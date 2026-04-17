ec0f5a3f9149dc31e0f38ce0026e56b7ab8a52f9
scholar-of-artifice <206404745+scholar-of-artifice@users.noreply.github.com>
2026-04-17 21:13:44 +0100

    add validation to the data transfer object

--- absurd-iguana/internal/models/event.go
+++ absurd-iguana/internal/models/event.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 // EventRequest represents the incoming JSON schema from the user/client
 type EventRequest struct {
@@ -13,4 +17,15 @@ type EventRequest struct {
 type EventResponse struct {
 	Status  string `json:"status"`
 	Message string `json:"message"`
+}
+
+// Validate checks if the required fields are present and well formed
+func (e *EventRequest) Validate() error {
+	if strings.TrimSpace(e.ID) == "" {
+		return errors.New("event ID is required.")
+	}
+	if strings.TrimSpace(e.Payload) == "" {
+		return errors.New("payload is required")
+	}
+	return nil
 }
