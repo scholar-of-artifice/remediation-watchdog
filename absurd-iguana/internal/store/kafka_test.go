d350fc1538ad036864147f8cc35e0c3e96b3560e
scholar-of-artifice <206404745+scholar-of-artifice@users.noreply.github.com>
2026-04-30 23:36:23 +0100

    add basic test for kafka store

---
+++ absurd-iguana/internal/store/kafka_test.go
@@ -0,0 +1,36 @@
+package store
+
+import (
+	"context"
+	"testing"
+)
+
+func TestKafkaStore_SaveEvent(t *testing.T) {
+	// setup: connect to the local Kafka container
+	addr := "dazzling-remora:9092" // move this to env var
+	topic := "integration-test-topic"
+	//
+	store, err := NewKafkaStore(addr, topic)
+	if err != nil {
+		t.Fatalf("Failed to create KafkaStore: %v", err)
+	}
+	defer store.Close()
+	//
+	ctx := context.Background()
+	// define test data
+	testID := "test-msg-123"
+	testData := map[string]string{"foo_event": "bar_test"}
+	// execute the test
+	t.Run("successfully is accepted into event stream", func(t *testing.T) {
+		err := store.SaveEvent(ctx, testID, testData)
+		if err != nil {
+			t.Fatalf("Failed to save event to Kafka: %v", err)
+		}
+		// synchronize: wait up to 2 seconds for broker to acknowledge the message
+		// verification
+		unread := store.Producer.Flush(2 * 1000)
+		if unread > 0 {
+			t.Errorf("Failed to flush all messages:\t %d remaining", unread)
+		}
+	})
+}
